23ee1e397fd3e58aa7501f553dbd5970d8ace178
nathanielgreen <ngreen2303@gmail.com>
2016-10-03 13:06:43 +0100

    Can add new logs

--- www/js/app.js
+++ www/js/app.js
@@ -1,33 +1,115 @@
 angular.module('bulletjournal', ['ionic'])
 
-.controller('DailyCtrl', function($scope, $ionicModal) {
-  $scope.tasks = [];
+.factory('Logs', function() {
+  return {
+    all: function() {
+      var logString = window.localStorage['logs'];
+      if(logString) {
+        return angular.fromJson(logString);
+      }
+      return [];
+    },
+    save: function(logs) {
+      window.localStorage['logs'] = angular.toJson(logs);
+    },
+    newLog: function(logTitle) {
+      // Add a new log
+      return {
+        title: logTitle,
+        tasks: []
+      };
+    },
+    getLastActiveIndex: function() {
+      return parseInt(window.localStorage['lastActiveLog']) || 0;
+    },
+    setLastActiveIndex: function(index) {
+      window.localStorage['lastActiveLog'] = index;
+    }
+  }
+})
 
-  // Create and load the Modal
-  $ionicModal.fromTemplateUrl('new-task.html', function(modal) { 
+.controller('DailyCtrl', function($scope, $timeout, $ionicModal, Logs, $ionicSideMenuDelegate) {
+
+  // A utility function for creating a new log
+  // with the given logTitle
+  var createLog = function(logTitle) {
+    var newLog = Logs.newLog(logTitle);
+    $scope.logs.push(newLog);
+    Logs.save($scope.logs);
+    $scope.selectLog(newLog, $scope.logs.length-1);
+  }
+
+
+  // Load or initialize logs
+  $scope.logs = Logs.all();
+
+  // Grab the last active, or the first log
+  $scope.activeLog = $scope.logs[Logs.getLastActiveIndex()];
+
+  // Called to create a new log
+  $scope.newLog = function() {
+    var logTitle = prompt('Log name');
+    if(logTitle) {
+      createLog(logTitle);
+    }
+  };
+
+  // Called to select the given log
+  $scope.selectLog = function(log, index) {
+    $scope.activeLog = log;
+    Logs.setLastActiveIndex(index);
+    $ionicSideMenuDelegate.toggleLeft(false);
+  };
+
+  // Create our modal
+  $ionicModal.fromTemplateUrl('new-task.html', function(modal) {
     $scope.taskModal = modal;
   }, {
-    scope: $scope,
-    animation: 'slide-in-up'
+    scope: $scope
   });
 
-  // Called when the form is submitted
   $scope.createTask = function(task) {
-    $scope.tasks.push({
+    if(!$scope.activeLog || !task) {
+      return;
+    }
+    $scope.activeLog.tasks.push({
       title: task.title
     });
     $scope.taskModal.hide();
+
+    // Inefficient, but save all the logs
+    Logs.save($scope.logs);
+
     task.title = "";
   };
 
-  // Open our new task modal
   $scope.newTask = function() {
     $scope.taskModal.show();
   };
 
-  // Close the new task modal
   $scope.closeNewTask = function() {
     $scope.taskModal.hide();
+  }
+
+  $scope.toggleLogs = function() {
+    $ionicSideMenuDelegate.toggleLeft();
   };
 
-});
+
+  // Try to create the first log, make sure to defer
+  // this by using $timeout so everything is initialized
+  // properly
+  $timeout(function() {
+    if($scope.logs.length == 0) {
+      while(true) {
+        var logTitle = prompt('Your first log title:');
+        if(logTitle) {
+          createLog(logTitle);
+          break;
+        }
+      }
+    }
+  }, 1000);
+
+})
+
